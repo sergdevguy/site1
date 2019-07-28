7c3f74059b0e6465049791e2728cfb0c4fcebad6
Sergdevguy <sergdevguy@gmail.com>
2019-07-28 21:34:34 +0300

    Work in process.

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,6 +1,6 @@
 // import required js-files
 //= ../../../node_modules/jquery/dist/jquery.js
-// ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
 
  
 // Import needed js-files from Bootstrap 4
