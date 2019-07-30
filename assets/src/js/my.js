690e740d2e8729adb86b367e0d1a229909f9e2ae
Sergdevguy <sergdevguy@gmail.com>
2019-07-30 16:14:57 +0300

    Complite animate step.

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -44,17 +44,17 @@ $( document ).ready(function(){
 
     /* Считаем цифры от 0 до указанного числа в элементе */
     function ticktick(htmlElem, numberFromArr){
+        var tick = 0;
         if(htmlElem.text() == 0){
-            var tick = 0;
             // начать повторы с интервалом
             var timerId = setInterval(function() {
                 if(numberFromArr > tick){
                     tick += 1;
                     htmlElem.text(tick);
-                } else{
+                } else if(numberFromArr == tick){
+                    clearInterval(timerId);
                     ticktick_ready += 1;
                     console.log("tick!");
-                    clearInterval(timerId);
                 }
             }, 10);
         }
@@ -65,18 +65,66 @@ $( document ).ready(function(){
     var windowHeight = $(window).height();
     var ticktick_ready = 0;
 
-    $(document).on('scroll', function() {
+    $(window).on('scroll', function() {
         $('.number').each(function(index) {
             var self = $(this);
             height = self.offset().top + self.height();
-            if ($(document).scrollTop() + windowHeight >= height) {
+            if ($(document, window).scrollTop() + windowHeight >= height ) {
                 ticktick(self, achivmentArr[index]);
             }
         });
         if(ticktick_ready >= 4){
-            $(document).unbind('scroll');
+            //$(document).unbind('scroll');
         }
     });
-    
 
+
+
+
+    /* Добавляем анимацию, когда элемент в области видимости экрана
+    _________________________________________________________
+    _________________________________________________________
+    _________________________________________________________
+    */
+    jQuery.fn.extend({
+        onAppearanceAddClass: function(class_to_add) {
+          var $window = $( window ),
+              window_height = $window.height(),
+              array_of_$elements = [];
+          this.each(function(i,el) {
+            array_of_$elements.push($( el ));
+          })
+          scrollHandler();
+              if (array_of_$elements.length) {
+            $window.on('resize', resizeHandler).on('resize', scrollHandler).on('scroll', scrollHandler);
+          }
+          function resizeHandler() {
+            window_height = $window.height();
+          }
+          function watchProcessedElements(array_of_indexes) {
+              var l, i;
+            for (l = array_of_indexes.length, i = l - 1; i > -1; --i) {
+              array_of_$elements.splice(array_of_indexes[i], 1);
+            }
+            if (!array_of_$elements.length) {
+              $window.off('resize', resizeHandler).off('scroll', scrollHandler).off('resize', scrollHandler);
+            }
+          }
+          function scrollHandler() {
+            var i, l, processed = [];
+            for ( l = array_of_$elements.length, i = 0; i < l; ++i ) {
+              if ($window.scrollTop() + window_height > array_of_$elements[i].offset().top) {
+                array_of_$elements[i].addClass(class_to_add);
+                processed.push(i); 
+              }
+            }
+            if (processed.length) {
+              watchProcessedElements(processed);
+            }
+          }
+          return this;
+        }
+      })
+      
+      $('.slideAnimated').onAppearanceAddClass('animated slideInLeft showSlide');
 });
